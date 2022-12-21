80d9902f2596a56d0f1236f18aedd5c845d77c45
Matt2991 <112526429+Matt2991@users.noreply.github.com>
2022-12-20 21:48:50 -0600

    count-up added

--- app.js
+++ app.js
@@ -27,9 +27,37 @@ var x = setInterval(function () {
   document.getElementById("demo").innerHTML =
     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
+  // 280 days from conception to birth
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "MY GIRL";
   }
+}, 1000);
+
+// start of counter
+// Set the date we're counting down to
+var dateOfConcept = new Date("Jul 16, 2022 06:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = now - dateOfConcept;
+
+  //console.log(distance);
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("concept").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // 280 days from conception to birth
 }, 1000);
